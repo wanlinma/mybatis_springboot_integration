370b017eca0f6fe33e62a62fb57d19b16ac73fa3
强哥 <13001001212@163.com>
2019-10-17 10:58:49 +0800

    小改

--- src/main/java/com/asiainfo/mybatis_demo/mapper/UserMapper.java
+++ src/main/java/com/asiainfo/mybatis_demo/mapper/UserMapper.java
@@ -12,5 +12,5 @@ public interface UserMapper {
 //    @Select("select * from tb_user")  //  注解开发和xml 开发只能选一种
     List<User> findAllUser();
     
-    List<User> findUserById(@Param("userId") Integer id);
+    List<User> findUserById(@Param("userId") Integer id);// 用 @Param 可以  select * from tb_user where user_id=#{userId} 此处 的 #{userId} 就是 @Param("userId") 所指定的，不加 @Param 那么，sql  的 参数需要 和 方法参数名一样
 }
